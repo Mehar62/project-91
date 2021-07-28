e1afedfdcd4adfa39b20d745e447eda273fb1db3
Mehar62 <78727941+Mehar62@users.noreply.github.com>
2021-07-28 14:24:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ player2_score = 0;
 document.getElementById("player1_name").innerHTML = player1_name + " : ";
 document.getElementById("player2_name").innerHTML = player2_name + " : ";
 
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+
 document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
 document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
 
